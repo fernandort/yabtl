c04d2ef35fe8d19f0a1a9a8808ab7318fd9db700
Jon Durbin <jon@jondurbin.com>
2012-09-30 12:09:52 -0400

    Updated test program.

--- src/yabtl.c
+++ src/yabtl.c
@@ -1,37 +1,37 @@
 #include <yabtl.h>
 #include <sys/time.h>
 
-#define COUNT 3
-#define TOTAL 10 //10000
+#define COUNT 64
+#define TOTAL 16777216
 
 int main( int argc, char *argv[] )
 {
   yabtl tree;
-  int i, v;
+  int i;
+  uint32_t v, l;
   struct timeval tv1, tv2;
   yabtl_item *item;
   yabtl_node *iter;
 
-  yabtl_init( &tree, COUNT, YABTL_INT );
+  yabtl_init( &tree, COUNT, YABTL_UINT32_T );
 
   gettimeofday(&tv1, NULL);
   for ( i = 0; i < TOTAL + 1; i++ )
   {
-    v = i; //TOTAL - i; //i; //10 - i;
-    item = yabtl_insert( &tree, ( void * )&v, ( void * )&i );
+    v = rand() % TOTAL;
+    l = v;
+    ( void )yabtl_insert( &tree, ( void * )&v, ( void * )&i );
   }
   gettimeofday(&tv2, NULL);
   printf ("Total time = %f seconds\n", (double) (tv2.tv_usec - tv1.tv_usec)/1000000 +  (double) (tv2.tv_sec - tv1.tv_sec));
 
-  i = 4;
   gettimeofday(&tv1, NULL);
-  item = yabtl_search( &tree, ( void * )&i );
+  item = yabtl_search( &tree, ( void * )&l );
   gettimeofday(&tv2, NULL);
   if ( item == NULL )
-    printf( "Failed to find %d\n", i );
+    printf( "Failed to find %ld\n", l );
   else
     printf( "Found: %d\n", *( int * )item->key );
   printf ("Total time = %f seconds\n", (double) (tv2.tv_usec - tv1.tv_usec)/1000000 +  (double) (tv2.tv_sec - tv1.tv_sec));
-
   return 0;
 }
