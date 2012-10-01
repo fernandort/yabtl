03732ef013048ae186f7cc64ec39ddd7450c0e22
Jon Durbin <jond@greenviewdata.com>
2012-10-01 11:58:18 -0400

    Updated test code.

--- src/yabtl.c
+++ src/yabtl.c
@@ -9,21 +9,25 @@ int main( int argc, char *argv[] )
   struct timeval tv1, tv2;
   yabtl_item *item;
   yabtl_node *iter;
-  uint32_t TOTAL = 15;
-  uint32_t ORDER = 3;
+  void *data;
+  uint32_t TOTAL = 4194304;
+  uint32_t ORDER = 17;
 
   yabtl_init( &tree, ORDER, YABTL_UINT32_T );
   srand( time( NULL ) );
   gettimeofday(&tv1, NULL);
   for ( i = 0; i < TOTAL; i++ )
   {
-    v = i; //rand() % TOTAL;
+    v = rand() % TOTAL;
     l = v;
-    void *data = malloc( sizeof( uint32_t ) );
+    data = malloc( sizeof( uint32_t ) );
     *( uint32_t * )data = v;
     item = yabtl_insert( &tree, ( void * )&v, data );
-    yabtl_iterate( &tree, tree.root );
-    printf( "\n" );
+    if ( item->data != data )
+    {
+      free( data );
+      data = NULL;
+    }
   }
   gettimeofday(&tv2, NULL);
   printf ("Total time to insert %d items: %f seconds\n", TOTAL, (double) (tv2.tv_usec - tv1.tv_usec)/1000000 +  (double) (tv2.tv_sec - tv1.tv_sec));
@@ -34,12 +38,9 @@ int main( int argc, char *argv[] )
   if ( item == NULL )
     printf( "Failed to find %d\n", l );
   else
-    printf( "Found: %d\n", *( int * )item->key );
+    printf( "Found key: %d\n", *( int * )item->key );
   printf ("Total time to query: %f seconds\n", (double) (tv2.tv_usec - tv1.tv_usec)/1000000 +  (double) (tv2.tv_sec - tv1.tv_sec));
 
-  yabtl_iterate( &tree, tree.root );
-  printf( "\n" );
-  //yabtl_destroy( &tree );
-
+  yabtl_destroy( &tree );
   return 0;
 }
