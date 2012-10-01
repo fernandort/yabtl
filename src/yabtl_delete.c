59eb29b4d6e735e911ac423947fed5584d1709a0
Jon Durbin <jond@greenviewdata.com>
2012-10-01 11:57:57 -0400

    Added comments to delete code.

--- src/yabtl_delete.c
+++ src/yabtl_delete.c
@@ -1,23 +1,31 @@
 #include <yabtl.h>
 
+// Delete a single item from a node.
 void yabtl_delete_item
 (
   yabtl_item **item
 )
 {
+  // Make sure the item isn't null already.
   if ( *item == NULL )
   {
     return;
   }
 
+  // Free the item's parts.
   free( ( *item )->key );
   free( ( *item )->data );
+
+  // Make sure we don't have hanging pointers.
   ( *item )->key = NULL;
   ( *item )->data = NULL;
+
+  // Free the item.
   free( *item );
   *item = NULL;
 }
 
+// Delete an entire node from the tree.
 void yabtl_delete_node
 (
   yabtl_node **node
@@ -25,49 +33,51 @@ void yabtl_delete_node
 {
   int i;
 
+  // Make sure the node isn't already null.
   if ( *node == NULL )
   {
     return;
   }
 
-  if ( ( *node )->child != NULL )
+  // Delete all of the child sub-trees.
+  for ( i = 0; i < ( *node )->count + 1; i++ )
   {
-    for ( i = 0; i < ( *node )->count + 1; i++ )
+    if ( ( *node )->child[i] != NULL )
     {
-      if ( ( *node )->child[i] != NULL )
-      {
-        yabtl_delete_node( ( yabtl_node ** )&( *node )->child[i] );
-      }
+      yabtl_delete_node( ( yabtl_node ** )&( *node )->child[i] );
     }
-    free( ( *node )->child );
-    ( *node )->child = NULL;
   }
+  free( ( *node )->child );
+  ( *node )->child = NULL;
 
-  if ( ( *node )->item != NULL )
+  // Delete all of the items.
+  for ( i = 0; i < ( *node )->count; i++ )
   {
-    for ( i = 0; i < ( *node )->count; i++ )
+    if ( ( *node )->item[i] != NULL )
     {
-      if ( ( *node )->item[i] != NULL )
-      {
-        yabtl_delete_item( ( yabtl_item ** )&( *node )->item[i] );
-      }
+      yabtl_delete_item( ( yabtl_item ** )&( *node )->item[i] );
     }
-    free( ( *node )->item );
-    ( *node )->item = NULL;
   }
+  free( ( *node )->item );
+  ( *node )->item = NULL;
 
+  // Free the memory used by the node.
   free( *node );
   *node = NULL;
 }
 
+// Recursively destroy an entire b-tree.
 void yabtl_destroy
 (
   yabtl *tree
 )
 {
+  // Make sure we weren't passed a null tree.
   if ( tree == NULL )
   {
     return;
   }
+
+  // Start at the root node and work our way down.
   yabtl_delete_node( &tree->root );
 }
