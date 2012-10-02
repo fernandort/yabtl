3e5ef2a6ff9a5c4addd6549a4d6c9f8f6f3db7dc
Jon Durbin <jond@greenviewdata.com>
2012-10-02 15:37:50 -0400

    More work on delete code.

--- src/yabtl_delete.c
+++ src/yabtl_delete.c
@@ -25,6 +25,60 @@ void yabtl_destroy_item
   *item = NULL;
 }
 
+// Merge the node origin into the node destination.
+void yabtl_merge_siblings
+(
+  yabtl_node **parent,
+  yabtl_node **destination,
+  yabtl_node **origin,
+  int index,
+  bool merging_left
+)
+{
+  int i;
+  yabtl_item *to_move;
+
+  // First, insert the parent item at index into the destination node.
+  if ( merging_left == true )
+  {
+    index--;
+    to_move = ( *parent )->item[index];
+  } else
+  {
+    to_move = ( *parent )->item[index];
+  }
+  ( *destination )->item[( *destination )->count++] = to_move;
+
+printf( "Moving %d from parent.\n", *( int * )to_move->key );
+printf( "Shifting elements from %d+ over.\n", index );
+
+  // Remove the item at index from parent.
+  for ( i = index; i < ( *parent )->count + 1; i++ )
+  {
+    ( *parent )->item[i] = ( *parent )->item[i + 1];
+    ( *parent )->child[i] = ( *parent )->child[i + 1];
+  }
+  ( *parent )->child[i] = ( *parent )->child[i + 1];
+  ( *parent )->count--;
+
+  // Move all of the children over.
+  for ( i = 0; i < ( *origin )->count + 1; i++ )
+  {
+    ( *destination )->child[( *destination )->count + i] = ( *origin )->child[i];
+    ( *origin )->child[i] = NULL;
+  }
+
+  // Add all of the items and children from origin to destination.
+  for ( i = 0; i < ( *origin )->count; i++ )
+  {
+    ( *destination )->item[( *destination )->count++] = ( *origin )->item[i];
+    ( *origin )->item[i] = NULL;
+  }
+
+  // Destroy the origin.
+  yabtl_destroy_node( origin );
+}
+
 bool yabtl_delete_recursive
 (
   yabtl *tree,
@@ -35,6 +89,7 @@ bool yabtl_delete_recursive
   int i;
   int index;
   bool result;
+  yabtl_node *left, *right;
 
   index = yabtl_binary_search( tree, *node, key );
 
@@ -45,7 +100,7 @@ bool yabtl_delete_recursive
     {
       // We're in a leaf node, so just remove it.
       yabtl_destroy_item( &( *node )->item[index] );
-      for ( i = index; i < ( *node )->count - 1; i++ )
+      for ( i = index; i < ( *node )->count; i++ )
       {
         ( *node )->item[i] = ( *node )->item[i + 1];
       }
@@ -70,10 +125,35 @@ bool yabtl_delete_recursive
     index = index * -1 - 1;
     if ( ( result = yabtl_delete_recursive( tree, ( yabtl_node ** )&( *node )->child[index], key ) ) == true )
     {
-      printf( "Child count: %d\n", ( ( yabtl_node * )( *node )->child[index] )->count );
-      printf( "Need to have at least %d\n", tree->order - 1 );
-      // We found the item and deleted it.
-      printf( "Found and removed, weeee!\n" );
+      if ( ( ( yabtl_node * )( *node )->child[index] )->count >= tree->order - 1 )
+      {
+        printf( "All done, child still has %d items.\n", ( ( yabtl_node * )( *node )->child[index] )->count );
+        return result;
+      }
+      if ( index - 1 >= 0 )
+      {
+        left = ( yabtl_node * )( *node )->child[index - 1];
+      } else
+      {
+        left = NULL;
+      }
+      right = ( yabtl_node * )( *node )->child[index + 1];
+      if ( left != NULL && left->count >= tree->order )
+      {
+        printf( "About to borrow from left sibling!\n" );
+      } else if ( right != NULL && right->count >= tree->order )
+      {
+        printf( "About to borrow from right sibling!\n" );
+      } else if ( left != NULL )
+      {
+        printf( "About to merge with left sibling!\n" );
+        yabtl_merge_siblings( node, ( yabtl_node ** )&( *node )->child[index - 1], ( yabtl_node ** )&( *node )->child[index], index, true );
+      } else if ( right != NULL )
+      {
+printf( "Origin: %d, Destination: %d\n", index - 1, index );
+        printf( "About to merge with right sibling!\n" );
+        yabtl_merge_siblings( node, ( yabtl_node ** )&( *node )->child[index], ( yabtl_node ** )&( *node )->child[index + 1], index, false );
+      }
     }
   }
 }
