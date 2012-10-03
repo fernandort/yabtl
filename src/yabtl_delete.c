b5f53d4ee1806582bb5076820277e82f5c481ba5
Jon Durbin <jond@greenviewdata.com>
2012-10-03 14:47:27 -0400

    Finished delete routine.

--- src/yabtl_delete.c
+++ src/yabtl_delete.c
@@ -132,7 +132,32 @@ void yabtl_merge_siblings
   yabtl_destroy_node( right );
 }
 
-bool yabtl_delete_recursive
+// Recursively find predecessor of an item.
+yabtl_item *yabtl_predecessor
+(
+  yabtl *tree,
+  yabtl_node **node
+)
+{
+  int i;
+
+  if ( ( *node )->leaf == false )
+  {
+    // Find the last child of this node.
+    i = ( *node )->count;
+    while ( ( *node )->child[i] == NULL )
+    {
+      i--;
+    }
+    return yabtl_predecessor( tree, ( yabtl_node ** )&( *node )->child[i] );
+  }
+
+  // We are in the leaf, return the last item.
+  return ( *node )->item[( *node )->count - 1];
+}
+
+// Recursively delete an item from the b-tree.
+yabtl_item *yabtl_delete_recursive
 (
   yabtl *tree,
   yabtl_node **node,
@@ -141,11 +166,13 @@ bool yabtl_delete_recursive
 {
   int i;
   int index;
-  bool result;
+  yabtl_item *result;
   int left_index, right_index;
   yabtl_node *left, *right;
   yabtl_node *new_node;
   yabtl_item *parent_item;
+  yabtl_item *predecessor;
+  yabtl_item *temp;
 
   index = yabtl_binary_search( tree, *node, key );
 
@@ -166,91 +193,110 @@ bool yabtl_delete_recursive
       ( *node )->count--;
 
       // Return true, indicating we successfully removed an item.
-      return true;
+      return NULL;
     } else
     {
-      // This is an internal node, need to do more stuff...
-      printf( "O noes, I'm in an internal node!  What a PITA\n" );
+      // Find the predecessor and return it.
+      return yabtl_predecessor( tree, ( yabtl_node **)&( *node )->child[index] );
     }
   } else
   {
     if ( ( *node )->leaf == true )
     {
       // We are in a leaf node, couldn't find it, return false.
-      return false;
+      return NULL;
     }
 
     // We didn't find the item, check child at the closest index.
     index = index * -1 - 1;
-    if ( ( result = yabtl_delete_recursive( tree, ( yabtl_node ** )&( *node )->child[index], key ) ) == true )
+    result = yabtl_delete_recursive( tree, ( yabtl_node ** )&( *node )->child[index], key );
+    if ( ( ( yabtl_node * )( *node )->child[index] )->count >= ( tree->order >> 1 ) )
     {
-      if ( ( ( yabtl_node * )( *node )->child[index] )->count >= ( tree->order >> 1 ) )
-      {
-        printf( "All done, child still has %d items.\n", ( ( yabtl_node * )( *node )->child[index] )->count );
-        return result;
-      }
-
-      // Set pointers to left and right siblings.
-      if ( index - 1 >= 0 )
-      {
-        left = ( yabtl_node * )( *node )->child[index - 1];
-      } else
-      {
-        left = NULL;
-      }
-      right = ( yabtl_node * )( *node )->child[index + 1];
-
-      if ( left != NULL && left->count > ( tree->order >> 1 ) )
-      {
-        // Shift an item from the left sibling into the current node.
-        yabtl_shift( tree, node, ( yabtl_node ** )&( *node )->child[index - 1], ( yabtl_node ** )&( *node )->child[index], index == ( *node )->count ? index - 1 : index, false );
-      } else if ( right != NULL && right->count > ( tree->order >> 1 ) )
-      {
-        // Shift an item from the right sibling into the current node.
-        yabtl_shift( tree, node, ( yabtl_node ** )&( *node )->child[index], ( yabtl_node ** )&( *node )->child[index + 1], index == ( *node )->count ? index - 1 : index, true );
-      } else
-      {
-        // If we can't borrow from a sibling, we need to merge two nodes, set the index of left and right siblings.
-        if ( left != NULL )
-        {
-          left_index = index - 1;
-          right_index = index;
-        } else
-        {
-          left_index = index;
-          right_index = index + 1;
-        }
-
-        // Create a new node.
-        new_node = malloc( sizeof( yabtl_node ) );
-        yabtl_init_node( tree, &new_node );
-
-        // Fix index of parent item to be moved.
-        if ( index > 0 )
-        {
-          index--;
-        }
-        // Set the pointer to the item that will be moved.
-        parent_item = ( *node )->item[index];
-
-        // Merge the siblings.
-        yabtl_merge_siblings( tree, &new_node, ( yabtl_node ** )&( *node )->child[left_index], ( yabtl_node ** )&( *node )->child[right_index], parent_item );
-
-        // Remove the parent item.
-        for ( i = index; i < ( *node )->count; i++ )
-        {
-          ( *node )->item[i] = ( *node )->item[i + 1];
-        }
-
-        // Move children over and insert new one.
-        ( *node )->child[index] = new_node;
-        for ( i = index + 1; i < ( *node )->count + 1; i++ )
-        {
-          ( *node )->child[i] = ( *node )->child[i + 1];
-        }
-      }
+      // The child at index still has enough keys, no need to restructure.
+      return result;
     }
-    return result;
+
+    // Set pointers to left and right siblings.
+    if ( index - 1 >= 0 )
+    {
+      left = ( yabtl_node * )( *node )->child[index - 1];
+    } else
+    {
+      left = NULL;
+    }
+    right = ( yabtl_node * )( *node )->child[index + 1];
+
+    if ( left != NULL && left->count > ( tree->order >> 1 ) )
+    {
+      // Shift an item from the left sibling into the current node.
+      yabtl_shift( tree, node, ( yabtl_node ** )&( *node )->child[index - 1], ( yabtl_node ** )&( *node )->child[index], index == ( *node )->count ? index - 1 : index, false );
+    } else if ( right != NULL && right->count > ( tree->order >> 1 ) )
+    {
+      // Shift an item from the right sibling into the current node.
+      yabtl_shift( tree, node, ( yabtl_node ** )&( *node )->child[index], ( yabtl_node ** )&( *node )->child[index + 1], index == ( *node )->count ? index - 1 : index, true );
+    } else
+    {
+      // If we can't borrow from a sibling, we need to merge two nodes, set the index of left and right siblings.
+      if ( left != NULL )
+      {
+        left_index = index - 1;
+        right_index = index;
+      } else
+      {
+        left_index = index;
+        right_index = index + 1;
+      }
+
+      // Create a new node.
+      new_node = malloc( sizeof( yabtl_node ) );
+      yabtl_init_node( tree, &new_node );
+      new_node->leaf = ( ( yabtl_node *)( *node )->child[left_index] )->leaf;
+
+      // Fix index of parent item to be moved.
+      if ( index > 0 )
+      {
+        index--;
+      }
+      // Set the pointer to the item that will be moved.
+      parent_item = ( *node )->item[index];
+
+      // Merge the siblings.
+      yabtl_merge_siblings( tree, &new_node, ( yabtl_node ** )&( *node )->child[left_index], ( yabtl_node ** )&( *node )->child[right_index], parent_item );
+
+      // Remove the parent item.
+      for ( i = index; i < ( *node )->count; i++ )
+      {
+        ( *node )->item[i] = ( *node )->item[i + 1];
+      }
+
+      // Move children over and insert new one.
+      ( *node )->child[index] = new_node;
+      for ( i = index + 1; i < ( *node )->count + 1; i++ )
+      {
+        ( *node )->child[i] = ( *node )->child[i + 1];
+      }
+
+      // Decrement count.
+      ( *node )->count--;
+    }
+  }
+  return result;
+}
+
+// Condense a root if necessary.
+void yabtl_condense_root
+(
+  yabtl *tree
+)
+{
+  yabtl_node *child;
+
+  if ( tree->root->count == 0 )
+  {
+    child = tree->root->child[0];
+    tree->root->child[0] = NULL;
+    yabtl_destroy_node( &tree->root );
+    tree->root = child;
   }
 }
 
@@ -261,7 +307,46 @@ bool yabtl_delete
   void *key
 )
 {
-  return yabtl_delete_recursive( tree, &tree->root, key );
+  int i, j;
+  yabtl_item *result;
+  yabtl_item *original;
+  yabtl_item *temp;
+  yabtl_node *child;
+
+  if ( ( result = yabtl_delete_recursive( tree, &tree->root, key ) ) != NULL )
+  {
+    // We are trying to delete an internal node, handle the logic here.
+    temp = malloc( sizeof( yabtl_item ) );
+    tree->copy_key( &temp, result->key );
+    temp->data = result->data;
+    result->data = NULL;
+
+    // The predecessor is stored in result, delete it from the tree.
+    yabtl_delete_recursive( tree, &tree->root, result->key );
+    yabtl_condense_root( tree );
+
+    // We removed the predecessor, now we need to find they original delete key and replace it.
+    original = yabtl_search( tree, key );
+    if ( original->key != NULL )
+    {
+      free( original->key );
+    }
+    if ( original->data != NULL )
+    {
+      free( original->data );
+    }
+    original->key = temp->key;
+    temp->key = NULL;
+    original->data = temp->data;
+    temp->data = NULL;
+
+    // Cleanup our temp stuff.
+    free( temp );
+    temp = NULL;
+  } else
+  {
+    yabtl_condense_root( tree );
+  }
 }
 
 // Delete an entire node from the tree.
